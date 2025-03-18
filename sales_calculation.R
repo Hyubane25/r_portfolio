507f765769b439d61ff02a7582f1a621f2b556c6
Janrey Ubane <janrey.ubane1998@gmail.com>
2025-03-18 19:24:40 +0800

    Added sales_calculation

---
+++ sales_calculation.R
@@ -0,0 +1,8 @@
+#our first calculations
+quarter_1_sales <- 2213.42
+quarter_2_sales <- 442323.32
+midyear_sales <- quarter_1_sales + quarter_2_sales
+yearend_sales <- midyear_sales * 2
+
+midyear_sales
+yearend_sales
