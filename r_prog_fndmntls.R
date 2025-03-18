70aac0541985d34b3903b5f08cd720eee7830153
Janrey Ubane <janrey.ubane1998@gmail.com>
2025-03-18 19:24:27 +0800

    Added r_prog_fndmntls

---
+++ r_prog_fndmntls.R
@@ -0,0 +1,17 @@
+View(penguins)
+#sample comment
+
+#variable value application using "<-"
+age <- 26
+
+#variable value application using "="
+name = "Janrey Ubane"
+
+#vector/array value application
+fruit = c("banana", "guava", "avocado")
+fruit
+
+sample <- as_date(today())
+print(sample)
+
+
